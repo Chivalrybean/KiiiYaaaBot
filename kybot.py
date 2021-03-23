bf373f1884487adb49ac708b797df6248db2b85e
Chivalrybean <jlowmaster@gmail.com>
2021-03-23 13:16:09 -0700

    Fixed errors.

--- kybot.py
+++ kybot.py
@@ -12,31 +12,32 @@ class Shot_tracker:
     def __init__(self, characters=[]):
         self.characters = characters
         self.shots = dict()
-        shots.update([(num, []) for num in range(-5, 21)])
+        self.shots.update([(num, []) for num in range(-5, 21)])
 
-    def move_character(character):
+    def move_character(self, character):
         pass
 
-    def roll_initiative():
+    def roll_initiative(self):
         pass
 
     def __repr__(self):
-        used_shots = [(shot, this.shots[shot]0 for shot in this.shots if this.shots[shot] != []]
+        used_shots = [(shot, self.shots[shot])
+                      for shot in self.shots if self.shots[shot] != []]
         used_shots.reverse()
-        shot_counter=""
+        shot_counter = ""
         if used_shots:
             for shot in used_shots:
                 shot_counter += f"\n {shot[0]}: {shot[1]}"
         else:
-            shot_counter="There's no one on the shot tracker"
+            shot_counter = "There's no one on the shot tracker"
         return shot_counter
 
 
 class Swerve:
     def __init__(self, die1, die2):
-        self.die1=die1
-        self.die2=die2
-        self.total=sum(die1) - sum(die2)
+        self.die1 = die1
+        self.die2 = die2
+        self.total = sum(die1) - sum(die2)
 
     def get_total(self):
         return self.total
@@ -47,19 +48,19 @@ class Swerve:
 
 class Action_check:
     def __init__(self, swerve, action_value, targets, defense, weapon_damage, toughness, comment):
-        self.swerve=swerve
-        self.action_value=action_value
-        self.targets=targets
-        self.defense=defense
-        self.weapon_damage=weapon_damage
-        self.toughness=toughness
-        self.comment=comment
-        self.result=self.generate_result()
+        self.swerve = swerve
+        self.action_value = action_value
+        self.targets = targets
+        self.defense = defense
+        self.weapon_damage = weapon_damage
+        self.toughness = toughness
+        self.comment = comment
+        self.result = self.generate_result()
     # When checking the various arguments, I don't need to do else statements, only ifs!
 
     def generate_result(self):
-        response=f"{self.swerve}"
-        current_total=self.swerve.get_total()
+        response = f"{self.swerve}"
+        current_total = self.swerve.get_total()
         if self.action_value is not None:
             current_total += self.action_value
             response += f" + Action Value of {self.action_value}"
@@ -95,14 +96,14 @@ class Action_check:
 
 def swerve_roller(die1, die2):
     while die1 == 6 and die2 == 6:
-        die1=d6()
-        die2=d6()
+        die1 = d6()
+        die2 = d6()
     if die1 == 6:
-        die1, die2=explode(), [die2]
+        die1, die2 = explode(), [die2]
     elif die2 == 6:
-        die1, die2=[die1], explode()
+        die1, die2 = [die1], explode()
     else:
-        die1, die2=[die1], [die2]
+        die1, die2 = [die1], [die2]
     return Swerve(die1, die2)
 
 
@@ -111,25 +112,25 @@ def d6():  # consider making this a more generic die, and the max value being an
 
 
 def explode():
-    die=6
-    die_pool=[6]
+    die = 6
+    die_pool = [6]
     while die == 6:
-        die=d6()
+        die = d6()
         die_pool.append(die)
     return die_pool
 
 
 def mooks(amount, action_value=8):
-    rolls=[]
-    mook=0
+    rolls = []
+    mook = 0
     try:
         while mook < amount:
-            die1=d6()
+            die1 = d6()
             if die1 == 6:
-                die1=sum(explode())
-            die2=d6()
+                die1 = sum(explode())
+            die2 = d6()
             if die2 == 6:
-                die2=sum(explode())
+                die2 = sum(explode())
             rolls.append(die1 - die2 + action_value)
             mook += 1
         return rolls
@@ -143,14 +144,14 @@ def initiative_roll(speed):
     """Rolls a d6 and adds the input speed value of the character (input from Discord command)"""
     if type(speed) != int:
         return f"Format for Initiative is `/init <intiger>` (your speed value was '{speed}')"
-    die=d6()
+    die = d6()
     return f"rolled [{die}] + {speed} = {die+speed}"
 
 
-guild_ids=[701612732062892153, 351522944461307915]
+guild_ids = [701612732062892153, 351522944461307915]
 
-client=commands.Bot(command_prefix="/")
-slash=SlashCommand(client, sync_commands=True)
+client = commands.Bot(command_prefix="/")
+slash = SlashCommand(client, sync_commands=True)
 
 
 @client.event
@@ -158,7 +159,7 @@ async def on_ready():
     print('The bot has logged in as {0.user}'.format(client))
     await client.change_presence(status=discord.Status.idle, activity=discord.Game("/fs to roll"))
 
-fs_options=[
+fs_options = [
     {
         "name": "action_value",
         "description": "Include the character's Action Value to the roll",
@@ -202,23 +203,23 @@ fs_options=[
 async def _fs(ctx, action_value=None, targets=None, defense=None, weapon_damage=None, toughness=None, comment=None):
     """Add in the default inputs, AV, etc. Completely rework handling the arguments here."""
     await ctx.respond()
-    channel=ctx.channel
-    user=ctx.author
-    die1=d6()
-    die2=d6()
+    channel = ctx.channel
+    user = ctx.author
+    die1 = d6()
+    die2 = d6()
     if die1 == 6 and die2 == 6:
         await channel.send(f"<@{user.id}> rolled Boxcars! Rerolling for a Way-Awesome Success, or Way-Awful Failure!")
         await channel.send("rerolling ...")
         await asyncio.sleep(3)
-        response=Action_check(swerve_roller(
+        response = Action_check(swerve_roller(
             die1, die2), action_value, targets, defense, weapon_damage, toughness, comment)
         await channel.send(f"<@{user.id}> rolled {response}")
     else:
-        response=Action_check(swerve_roller(
+        response = Action_check(swerve_roller(
             die1, die2), action_value, targets, defense, weapon_damage, toughness, comment)
         await channel.send(f"<@{user.id}> rolled {response}")
 
-mook_options=[
+mook_options = [
     {
         "name": "amount",
         "description": "How many times to roll? (default is 1)",
@@ -237,11 +238,11 @@ mook_options=[
 @slash.slash(name="mooks", description="Roll some mook rolls!", options=mook_options, guild_ids=guild_ids)
 async def _mooks(ctx, amount=1, action_value=8):
     await ctx.respond()
-    channel=ctx.channel
+    channel = ctx.channel
     await channel.send(f"{mooks(amount, action_value)}")
 
 
-init_options=[
+init_options = [
     {
         "name": "speed",
         "description": "The speed value of your character",
@@ -254,8 +255,8 @@ init_options=[
 @slash.slash(name="init", description="Roll for initiative (Feng Shui 2)!", options=init_options, guild_ids=guild_ids)
 async def _mooks(ctx, speed):
     await ctx.respond()
-    channel=ctx.channel
-    user=ctx.author
+    channel = ctx.channel
+    user = ctx.author
     await channel.send(
         f"<@{user.id}> rolled {initiative_roll(speed)} for initiative.")
 
@@ -263,8 +264,8 @@ async def _mooks(ctx, speed):
 @slash.slash(name="d6", description="Roll a six-sided die, for fortune, or other abilities", guild_ids=guild_ids)
 async def _d6(ctx):
     await ctx.respond()
-    channel=ctx.channel
-    user=ctx.author
+    channel = ctx.channel
+    user = ctx.author
     await channel.send(f"<@{user.id}> rolled a {d6()}.")
 
 client.run(ls.token)
